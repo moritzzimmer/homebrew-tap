47a6f13946b2a411debecbb769391ff184874277
goreleaserbot <goreleaser@carlosbecker.com>
2020-12-17 10:07:02 +0100

    Brew formula update for func version v0.0.20

--- func.rb
+++ func.rb
@@ -2,16 +2,16 @@
 class Func < Formula
   desc ""
   homepage ""
-  version "0.0.19"
+  version "0.0.20"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/moritzzimmer/func/releases/download/v0.0.19/func_0.0.19_darwin_amd64.tar.gz"
-    sha256 "ce2b26f5c0242059acf0e59e6c1528c25307f8412246b75406937882c76f3f90"
+    url "https://github.com/moritzzimmer/func/releases/download/v0.0.20/func_0.0.20_darwin_amd64.tar.gz"
+    sha256 "935b568b90bafe9e5f2765c394330f8a76727641a5699d79cac92554396cbc42"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/moritzzimmer/func/releases/download/v0.0.19/func_0.0.19_linux_amd64.tar.gz"
-    sha256 "31f09f4423717e658deeb0036de315a4919d50c6deb944752d4dc7e1aa0af21e"
+    url "https://github.com/moritzzimmer/func/releases/download/v0.0.20/func_0.0.20_linux_amd64.tar.gz"
+    sha256 "eeb7daf433524745c1b243b25c62e32483109734085d82c410c2ca415674109d"
   end
 
   def install
